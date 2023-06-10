b03690279ed5746a59333202695f338160620c4f
fav_dev <fezekannagha@gmail.com>
2023-06-10 03:40:29 +0100

    "I was able to create the first and section of the code for week4 the first script was changeImage.py to convert image and supplied_image_upload.py to upload the image and i also created their test cases"

--- week4/python_code/supplier_image_upload.py
+++ week4/python_code/supplier_image_upload.py
@@ -4,26 +4,14 @@ import requests
 # This example shows how a file can be uploaded using
 # The Python Requests module
 
-url = "http://localhost/upload/"
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/001.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/002.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/003.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/004.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/005.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/006.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/007.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/008.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/009.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
-with open('/home/student-01-a0a7adef2f7c/supplier-data/images/010.jpeg', 'rb') as opened:
-    r = requests.post(url, files={'file': opened})
+def upload_file(url, file_path):
+    with open(file_path, 'rb') as file:
+        response = requests.post(url, files={'file': file})
+        return response
 
-
+if __name__ == '__main__':
+    url = "http://localhost/upload/"
+    image_path = '/home/student-01-a0a7adef2f7c/supplier-data/images'
+    for image in image_path:
+        response = upload_file(url, image_path)
+        print(response.status_code)
